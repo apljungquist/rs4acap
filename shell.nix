4cb917a01c7f65bbf60fb9593ecc9fad8245f57b
AP Ljungquist <ap@ljungquist.eu>
2025-04-23 21:39:43 +0200

    chore: Clean up dev env
    These were made redundant by #10.

--- shell.nix
+++ shell.nix
@@ -5,18 +5,14 @@ in
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
-    avahi
     cargo
     clang
     clippy
     fd
     git
-    llvmPackages.libclang
     mkhelp
     nixfmt-rfc-style
     rustc
     rustfmt
   ];
-
-  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 }
