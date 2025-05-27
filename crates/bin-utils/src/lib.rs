cd4e78345a64d4a085322283408b39c9e07f3634
AP Ljungquist <ap@ljungquist.eu>
2025-05-27 20:21:17 +0200

    feat: Add completions command to all binaries

---
+++ crates/bin-utils/src/lib.rs
@@ -0,0 +1,2 @@
+//! Code that should be the same across all binaries in this project.
+pub mod completions_command;
--- crates/device-finder/src/main.rs
+++ crates/device-finder/src/main.rs
@@ -1,13 +1,43 @@
 #![forbid(unsafe_code)]
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use rs4a_bin_utils::completions_command::CompletionsCommand;
 
 use crate::commands::discover_devices::DiscoverDevicesCommand;
 
 mod commands;
 
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+impl Cli {
+    pub async fn exec(self) -> anyhow::Result<()> {
+        match self.command.unwrap_or_default() {
+            Commands::DiscoverDevices(cmd) => cmd.exec().await?,
+            Commands::Completions(cmd) => cmd.exec::<Self>()?,
+        }
+        Ok(())
+    }
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Discover devices on the local network
+    DiscoverDevices(DiscoverDevicesCommand),
+    Completions(CompletionsCommand),
+}
+
+impl Default for Commands {
+    fn default() -> Self {
+        Self::DiscoverDevices(DiscoverDevicesCommand::default())
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    DiscoverDevicesCommand::parse().exec().await
+    Cli::parse().exec().await
 }
