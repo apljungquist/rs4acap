3c9e760c6ecb46f7fd79032bd5b7a680e68ff999
AP Ljungquist <ap@ljungquist.eu>
2025-09-02 21:59:12 +0200

    chore: Add smoke tests for VAPIX bindings
    I try to write code that fails if any assumptions are violated and I experience that regressions oftentimes cause code that should return `Ok` to either return an `Err` or `panic!`. These tests leverage that to achieve a lot of test coverage, cheaply.

---
+++ crates/vapix/tests/smoke_tests.rs
@@ -0,0 +1,40 @@
+use std::env;
+
+use rs4a_vapix::{Client, ClientBuilder};
+
+async fn test_client() -> Option<Client> {
+    if env::var_os("AXIS_DEVICE_IP").is_some() {
+        Some(
+            ClientBuilder::from_env()
+                .unwrap()
+                .with_inner(|b| b.danger_accept_invalid_certs(true))
+                .build_with_automatic_scheme()
+                .await
+                .unwrap(),
+        )
+    } else {
+        eprintln!("No device configured, skipping test.");
+        None
+    }
+}
+
+#[tokio::test]
+async fn basic_device_info_get_all_unrestricted_properties_returns_ok() {
+    let Some(client) = test_client().await else {
+        return;
+    };
+    client
+        .basic_device_info_1()
+        .get_all_unrestricted_properties()
+        .send()
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn system_ready_system_ready_returns_ok() {
+    let Some(client) = test_client().await else {
+        return;
+    };
+    client.system_ready_1().system_ready().send().await.unwrap();
+}
