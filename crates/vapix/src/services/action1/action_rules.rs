5db43600121871dafacaa5f40026e720cbd81200
AP Ljungquist <ap@ljungquist.eu>
2025-09-10 14:28:52 +0200

    feat(vapix)!: Build all XML for `add_action_rule` (#38)
    The most difficult thing with using the SOAP style APIs, after properly
    parsing error responses, is crafting an XML request that the server will
    accept. As such this API does not make much sense if the user has to
    craft the part of the request themselves.
    
    One downside is that the new API probably does not support all valid
    requests that a user may want to send, but considering that I mean for
    the crate to also provide an API for robust response handling I think
    falling back on that should be an acceptable fallback.
    
    ---
    
    `crates/vapix/src/services/action1/action_rules.rs`:
    - Build the XML manually because this is easier in the short term than
    creating a type that serializes correctly, since the latter still
    requires a lot of trial and error for me. I am also suspicious of the
    storage footprint of serde trait implementations and the traits feel
    even more like a leaky abstraction when working with xml.
    - Accept the topic expression and message content as strings because I
    don't have a good guess for how these would be further decomposed. It
    would however be nice to provide a builder API for them, too.
    
    `crates/vapix/tests/serde.rs`:
    - Update the serde test to create a valid request that exercises more of
    the API. We know the request is valid because the same request is
    constructed in the smoke tests.

--- crates/vapix/src/services/action1/action_rules.rs
+++ crates/vapix/src/services/action1/action_rules.rs
@@ -1,5 +1,86 @@
 use serde::Deserialize;
 
+use crate::{
+    soap::SimpleRequest,
+    soap_http::{SoapHttpRequest, SoapRequest},
+};
+
+pub struct AddActionRuleRequest {
+    pub name: String,
+    pub enabled: bool,
+    pub conditions: Conditions,
+    pub primary_action: u16,
+}
+
+impl AddActionRuleRequest {
+    pub(crate) fn new(name: String, primary_action: u16) -> Self {
+        Self {
+            name,
+            enabled: true,
+            conditions: Conditions {
+                condition: Vec::new(),
+            },
+            primary_action,
+        }
+    }
+
+    pub fn enabled(mut self, enabled: bool) -> Self {
+        self.enabled = enabled;
+        self
+    }
+
+    pub fn condition(mut self, condition: Condition) -> Self {
+        self.conditions.condition.push(condition);
+        self
+    }
+}
+
+impl SoapRequest for AddActionRuleRequest {
+    fn to_envelope(self) -> anyhow::Result<String> {
+        let Self {
+            name,
+            enabled,
+            conditions,
+            primary_action,
+        } = self;
+        let mut params = String::new();
+        params.push_str(r#"<NewActionRule xmlns:tns1="http://www.onvif.org/ver10/topics" xmlns:tnsaxis="http://www.axis.com/2009/event/topics">"#);
+        params.push_str(r#"<Name>"#);
+        params.push_str(&name);
+        params.push_str(r#"</Name>"#);
+        params.push_str(r#"<Enabled>"#);
+        params.push_str(&enabled.to_string());
+        params.push_str(r#"</Enabled>"#);
+        params.push_str(r#"<Conditions>"#);
+        for condition in conditions.condition {
+            let Condition {
+                topic_expression,
+                message_content,
+            } = condition;
+            params.push_str(r#"<Condition>"#);
+            params.push_str(r#"<TopicExpression Dialect="http://docs.oasis-open.org/wsn/t-1/TopicExpression/Concrete" xmlns="http://docs.oasis-open.org/wsn/b-2">"#);
+            params.push_str(&topic_expression);
+            params.push_str(r#"</TopicExpression>"#);
+            params.push_str(r#"<MessageContent Dialect="http://www.onvif.org/ver10/tev/messageContentFilter/ItemFilter" xmlns="http://docs.oasis-open.org/wsn/b-2">"#);
+            params.push_str(&message_content);
+            params.push_str(r#"</MessageContent>"#);
+            params.push_str(r#"</Condition>"#);
+        }
+        params.push_str(r#"</Conditions>"#);
+        params.push_str(r#"<PrimaryAction>"#);
+        params.push_str(&primary_action.to_string());
+        params.push_str(r#"</PrimaryAction>"#);
+        params.push_str(r#"</NewActionRule>"#);
+        SimpleRequest::<()>::new("http://www.axis.com/vapix/ws/action1", "AddActionRule")
+            .params(params)
+            .to_envelope()
+    }
+}
+
+impl SoapHttpRequest for AddActionRuleRequest {
+    type Data = AddActionRuleResponse;
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct AddActionRuleResponse {
