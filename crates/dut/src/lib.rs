b77e7c85c7a5544236bb0fc166cfd543a3e142a2
AP Ljungquist <ap@ljungquist.eu>
2025-09-11 23:57:33 +0200

    fix: Export unsets unused environment variables
    I found that when switching from a VLT device to a local device, I often forgot to unset the custom ports. Even if I remember, it is one manual step that I'd rather not have to do.

--- crates/dut/src/lib.rs
+++ crates/dut/src/lib.rs
@@ -72,7 +72,7 @@ impl Device {
         }
     }
 
-    pub fn to_env(&self) -> Vec<(String, String)> {
+    pub fn to_env(&self) -> Vec<(String, Option<String>)> {
         let Self {
             host,
             username,
@@ -83,19 +83,34 @@ impl Device {
         } = self;
         let mut envs = Vec::new();
 
-        envs.push(("AXIS_DEVICE_IP".to_string(), host.to_string()));
-        envs.push(("AXIS_DEVICE_USER".to_string(), username.to_string()));
-        envs.push(("AXIS_DEVICE_PASS".to_string(), password.to_string()));
+        envs.push(("AXIS_DEVICE_IP".to_string(), Some(host.to_string())));
+
+        envs.push(("AXIS_DEVICE_USER".to_string(), Some(username.to_string())));
+
+        envs.push(("AXIS_DEVICE_PASS".to_string(), Some(password.to_string())));
+
         if let Some(p) = ssh_port {
-            envs.push(("AXIS_DEVICE_SSH_PORT".to_string(), p.to_string()));
+            envs.push(("AXIS_DEVICE_SSH_PORT".to_string(), Some(p.to_string())));
+        } else {
+            envs.push(("AXIS_DEVICE_SSH_PORT".to_string(), None));
         }
+
         if let Some(p) = http_port {
-            envs.push(("AXIS_DEVICE_HTTP_PORT".to_string(), p.to_string()));
+            envs.push(("AXIS_DEVICE_HTTP_PORT".to_string(), Some(p.to_string())));
+        } else {
+            envs.push(("AXIS_DEVICE_HTTP_PORT".to_string(), None));
         }
+
         if let Some(p) = https_port {
-            envs.push(("AXIS_DEVICE_HTTPS_PORT".to_string(), p.to_string()));
+            envs.push(("AXIS_DEVICE_HTTPS_PORT".to_string(), Some(p.to_string())));
+        } else {
+            envs.push(("AXIS_DEVICE_HTTPS_PORT".to_string(), None));
         }
-        envs.push(("AXIS_DEVICE_HTTPS_SELF_SIGNED".to_string(), "1".to_string()));
+
+        envs.push((
+            "AXIS_DEVICE_HTTPS_SELF_SIGNED".to_string(),
+            Some("1".to_string()),
+        ));
 
         envs
     }
