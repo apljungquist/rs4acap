d93cc854e07ca9fc2c114ae0ba2272ec6824d24e
AP Ljungquist <ap@ljungquist.eu>
2025-10-06 20:27:39 +0200

    feat(di): Add status column to list command output (#56)
    This is helpful for checking which device will be used when running
    other commands, and for inspecting the effect of other commands.
    
    Prompted by me adding an abandon command which definitely should
    deactivate the device, but I had forgotten to add it and was unsure if I
    needed to or if it was already implied by remove.
    
    Note that for better or worse, removing a device does not deactivate it.
    If remove ends up being more of a plumbing command, then perhaps it
    makes sense. If not, then it's probably surprising. In any case leave
    the current behavior intact until I have a stronger opinion.
    
    `bin/device-inventory-smoke-test.sh`:
    - Consolidate the list commands to make the output easier to compare and
    correlate with the commands that changed it.
    - Activate a device using environment variables before listing to make
    sure the output is reproducible in development where there may be an
    active device configured on the filesystem.
    
    `crates/device-inventory/src/commands/list.rs`:
    - The ad-hoc fingerprinting in two places is ugly, but it works and I
    would rather take on a little bit of tech debt now and then figure out
    the device data model later when there is a bit more code to infer
    patterns from.
    - No two correctly configured devices will have the same IP and HTTP
    port, or traffic could not be routed. There could of course be problems
    with obsolete information in the database so maybe this is a heuristic I
    will need to reconsider.

--- crates/device-inventory/src/commands/list.rs
+++ crates/device-inventory/src/commands/list.rs
@@ -19,25 +19,62 @@ impl ListCommand {
         let mut sorted_devices: Vec<_> = devices.into_iter().collect();
         sorted_devices.sort_by(|(left, _), (right, _)| left.cmp(right));
 
+        let mut statuses = vec!["STATUS".to_string()];
         let mut aliases = vec!["ALIAS".to_string()];
         let mut models = vec!["MODEL".to_string()];
         let mut hosts = vec!["HOST".to_string()];
-        for (alias, Device { host, model, .. }) in sorted_devices.into_iter() {
+
+        let active = rs4a_dut::Device::from_anywhere()?;
+        if let Some(active) = active.as_ref() {
+            let device = sorted_devices.iter().find(|(_, d)| {
+                d.host == active.host && d.http_port.unwrap_or(80) == active.http_port.unwrap_or(80)
+            });
+
+            statuses.push("ACTIVE".to_string());
+            aliases.push(device.map(|(a, _)| a.clone()).unwrap_or_default());
+            models.push(
+                device
+                    .map(|(_, d)| d.model.clone().unwrap_or_default())
+                    .unwrap_or_default(),
+            );
+            hosts.push(active.host.to_string());
+        }
+
+        for (
+            alias,
+            Device {
+                host,
+                model,
+                http_port,
+                ..
+            },
+        ) in sorted_devices.into_iter()
+        {
+            if let Some(d) = active.as_ref() {
+                if d.host == host && d.http_port.unwrap_or(80) == http_port.unwrap_or(80) {
+                    continue;
+                }
+            }
+
+            statuses.push("".to_string());
             aliases.push(alias);
             models.push(model.unwrap_or_default());
             hosts.push(host.to_string());
         }
 
+        let statuses_width = 1 + statuses.iter().map(|s| s.len()).max().unwrap();
         let aliases_width = 1 + aliases.iter().map(|s| s.len()).max().unwrap();
         let models_width = 1 + models.iter().map(|s| s.len()).max().unwrap();
 
-        // TODO: Consider showing which device is active and figuring out the testing.
-        for ((alias, model), host) in aliases
+        for (((status, alias), model), host) in statuses
             .into_iter()
+            .zip(aliases.into_iter())
             .zip(models.into_iter())
             .zip(hosts.into_iter())
         {
-            println!("{alias:aliases_width$} {model:models_width$} {host}");
+            println!(
+                "{status:statuses_width$} {alias:aliases_width$} {model:models_width$} {host}"
+            );
         }
 
         Ok(())
