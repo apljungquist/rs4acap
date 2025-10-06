3f4dd03a8dd8b78b05e41f0fc2c33f01229e2bd5
AP Ljungquist <ap@ljungquist.eu>
2025-10-06 18:54:10 +0200

    chore: Streamline examples
    Having examples in fewer places makes them easier to keep track of and the snapshot tests only use one file, so it is not enough to establish a memorable pattern.

--- bin/device-inventory-smoke-test.sh
+++ bin/device-inventory-smoke-test.sh
@@ -6,7 +6,7 @@ export DEVICE_INVENTORY_LOCATION=$(mktemp -d)
 export DEVICE_INVENTORY_OFFLINE=true
 
 set -x
-device-inventory import --source=json < crates/device-inventory/test-data/get-loans-response.json
+device-inventory import --source=json < crates/vlt/tests/responses/get_loans_non_empty.json
 device-inventory add local 192.168.0.90 root pass
 device-inventory list
 device-inventory activate --alias local --destination environment
