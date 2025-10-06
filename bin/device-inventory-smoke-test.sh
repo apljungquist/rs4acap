104edec02bbb6a667400e6d30b09697f06536126
AP Ljungquist <ap@ljungquist.eu>
2025-10-06 20:06:01 +0200

    feat(di): Add status column to list command output
    This is helpful for checking which device will be used when running other commands, and for inspecting the effect of other commands.
    
    Prompted by me adding an abandon command which definitely should deactivate the device, but I had forgotten to add it and was unsure if I needed to or if it was already implied by remove.
    
    Note that for better or worse, removing a device does not deactivate it. If remove ends up being more of a plumbing command, then perhaps it makes sense. If not, then it's probably surprising. In any case leave the current behavior intact until I have a stronger opinion.

--- bin/device-inventory-smoke-test.sh
+++ bin/device-inventory-smoke-test.sh
@@ -8,8 +8,11 @@ export DEVICE_INVENTORY_OFFLINE=true
 set -x
 device-inventory import --source=json < crates/device-inventory/test-data/get-loans-response.json
 device-inventory add local 192.168.0.90 root pass
-device-inventory list
-device-inventory activate --alias local --destination environment
 device-inventory for-each sh -- -c 'echo $AXIS_DEVICE_IP'
+device-inventory activate --alias local --destination environment
+eval $(device-inventory activate --alias local --destination environment)
+device-inventory list
+device-inventory remove --alias 'local'
+device-inventory list
 device-inventory remove --alias 'vlt-*'
 device-inventory list
