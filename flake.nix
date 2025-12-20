c80b9964bce20f7fad85d12bf9b4bb6255677ec9
AP Ljungquist <ap@ljungquist.eu>
2025-12-20 11:30:38 +0100

    chore: Adopt nix flakes
    This change is motivated by two needs that flakes appear to facilitate:
    - Provide a Rust installation that can be used by Rust Rover [^1]
    - Deduplicate the package definition for `mkhelp` which I have been
      copying to more and more repos.
    
    ---
    
    `.github/workflows/main.yaml`:
    - Combine install and verify steps because `exit` does not exist and
      since `make` should execute very quickly we maintain the ability to
      measure the time it takes to install dependencies, which is why the
      separate install step was added to begin with.
    
    `pkgs/mkhelp/default.nix`:
    - Removed since it is now fetched from apljungquist/mkhelp-rs.
    
    `rust-toolchain.toml`:
    - Use version 1.82.0 because that is what `shell.nix` ended up resolving to.
    - `rust-src` is needed for the standard library to be available for Rust Rover to use at the expected path [^1].
    - Export `RUST_SRC_PATH` because rust-analyzer and other tools need it [^1] and it makes it easier for me to find the path to enter into the Rust Rover settings.
    - Don't create any symlinks because I don't want to have effects outside of the repo and I don't know how much of a problem the changing paths will actually be.
    
    [^1]: https://nixos.wiki/wiki/Rust_Rover

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "rs4a developer environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    mkhelp = {
+      url = "github:apljungquist/mkhelp-rs/nix";
+      inputs.rust-overlay.follows = "rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      mkhelp,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            clang
+            fd
+            git
+            mkhelp.packages.${system}.default
+            nixfmt-rfc-style
+            rustToolchain
+          ];
+
+          shellHook = ''
+            # Prevent cargo from finding programs in the default cargo home by **appending** it to the path because
+            # otherwise cargo will **prepend** it to the path e.g. when looking for clippy.
+            export PATH="$PATH:$HOME/.cargo/bin"
+            # Tell rust-analyzer where to find the standard library
+            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
+          '';
+        };
+      }
+    );
+}
