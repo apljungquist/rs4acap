apljungquist/rs4acap

commit 622fa9dbd70547b54b4befc348af71eab0e72a58

feat(vapix)!: Remove `RestHttp*` traits (#110)
This shares many of the benefits from
26571f6d296c44c486c92e6a440dd645c8c0caf3

`crates/vapix/src/config/recording_group_1.rs`,
`crates/vapix/src/config/remote_object_storage_1.rs`
`crates/vapix/src/config/ssh_1.rs`:
- Derive `Serialize` to allow the lossless checking path to be used.
This isn't strictly necessary for this change since the individual
`send` implementations allow each request builder to choose which path
to use.

crates/vapix/src/config/recording_group_1.rs
@@ -1,11 +1,11 @@
 //! The [Recording group API].
 //!
 //! [Recording group API]: https://developer.axis.com/vapix/device-configuration/recording-group/
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::rest_http::RequestBuilder;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct CreateRecordingGroupResponse {
     pub id: String,
 }

crates/vapix/src/config/remote_object_storage_1.rs
@@ -1,9 +1,9 @@
 //! The remote object storage API (beta)
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::rest_http::RequestBuilder;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct CreateDestinationResponse {
     pub id: String,
 }

crates/vapix/src/config/remote_object_storage_1_beta.rs
@@ -6,7 +6,10 @@ use reqwest::Method;
 use serde_json::json;
 use url::Url;
 
-use crate::{http::Request, rest_http2::RestHttp2};
+use crate::{
+    http::{Error, HttpClient, Request},
+    rest, rest_http,
+};
 
 const BASE_PATH: &str = "config/rest/remote-object-storage/v1beta/destinations";
 
@@ -113,9 +116,7 @@ impl CreateDestinationRequest {
         self.data.description = Some(description);
         self
     }
-}
 
-impl CreateDestinationRequest {
     pub fn azure(id: DestinationId, azure: AzureDestination) -> Self {
         Self {
             data: CreateDestinationData {
@@ -137,16 +138,19 @@ impl CreateDestinationRequest {
             },
         }
     }
-}
 
-impl RestHttp2 for CreateDestinationRequest {
-    type ResponseData = DestinationData;
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         // PANICS:
         // The `unwrap` will never panic because `self.data` can always be serialized to JSON.
         Request::json(Method::POST, BASE_PATH.to_string())
             .body(serde_json::to_string_pretty(&json!({"data":self.data})).unwrap())
+    }
+
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<DestinationData, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
@@ -157,13 +161,16 @@ impl ListDestinationsRequest {
     pub fn new() -> Self {
         Self
     }
-}
 
-impl RestHttp2 for ListDestinationsRequest {
-    type ResponseData = Vec<DestinationData>;
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         Request::no_content(Method::GET, BASE_PATH.to_string())
+    }
+
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<Vec<DestinationData>, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
@@ -190,12 +197,8 @@ impl UpdateDestinationRequest {
             data: serde_json::Value::String(description),
         }
     }
-}
 
-impl RestHttp2 for UpdateDestinationRequest {
-    type ResponseData = ();
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         // PANICS:
         // The `unwrap` will never panic because `self.data` is a `serde_json::Value` which can
         // always be serialized to JSON.
@@ -204,6 +207,10 @@ impl RestHttp2 for UpdateDestinationRequest {
             format!("{BASE_PATH}/{}/{}", self.id.into_string(), self.property),
         )
         .body(serde_json::to_string_pretty(&json!({"data":self.data})).unwrap())
+    }
+
+    pub async fn send(self, client: &(impl HttpClient + Sync)) -> Result<(), Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
@@ -216,15 +223,15 @@ impl DeleteDestinationRequest {
     pub fn new(id: DestinationId) -> Self {
         Self { id }
     }
-}
 
-impl RestHttp2 for DeleteDestinationRequest {
-    type ResponseData = ();
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         Request::no_content(
             Method::DELETE,
             format!("{BASE_PATH}/{}", self.id.into_string()),
         )
+    }
+
+    pub async fn send(self, client: &(impl HttpClient + Sync)) -> Result<(), Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }

crates/vapix/src/config/siren_and_light_2_alpha.rs
@@ -3,7 +3,10 @@
 use reqwest::Method;
 use serde_json::json;
 
-use crate::{http::Request, rest_http2::RestHttp2};
+use crate::{
+    http::{Error, HttpClient, Request},
+    rest, rest_http,
+};
 
 const BASE_PATH: &str = "config/rest/siren-and-light/v2alpha";
 
@@ -36,13 +39,16 @@ impl GetMaintenanceModeRequest {
     pub fn new() -> Self {
         Self
     }
-}
 
-impl RestHttp2 for GetMaintenanceModeRequest {
-    type ResponseData = MaintenanceModeData;
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         Request::no_content(Method::GET, format!("{BASE_PATH}/maintenanceMode"))
+    }
+
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<MaintenanceModeData, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
@@ -57,16 +63,19 @@ impl StartMaintenanceModeRequest {
             data: StartMaintenanceModeData::default(),
         }
     }
-}
 
-impl RestHttp2 for StartMaintenanceModeRequest {
-    type ResponseData = EmptyData;
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         // PANICS:
         // The `unwrap` will never panic because `self.data` can always be serialized to JSON.
         Request::json(Method::POST, format!("{BASE_PATH}/maintenanceMode/start"))
             .body(serde_json::to_string_pretty(&json!({"data": self.data})).unwrap())
+    }
+
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<EmptyData, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
@@ -77,15 +86,18 @@ impl StopMaintenanceModeRequest {
     pub fn new() -> Self {
         Self
     }
-}
 
-impl RestHttp2 for StopMaintenanceModeRequest {
-    type ResponseData = EmptyData;
-
-    fn to_request(self) -> Request {
+    pub fn into_request(self) -> Request {
         // PANICS:
         // The `unwrap` will never panic because the body is a static JSON value.
         Request::json(Method::POST, format!("{BASE_PATH}/maintenanceMode/stop"))
             .body(serde_json::to_string_pretty(&json!({"data": {}})).unwrap())
+    }
+
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<EmptyData, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }

crates/vapix/src/config/ssh_1.rs
@@ -6,8 +6,12 @@
 //! [v2]: https://developer.axis.com/vapix/device-configuration/ssh-management/
 use reqwest::Method;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
-use crate::rest_http::RestHttp;
+use crate::{
+    http::{Error, HttpClient, Request},
+    rest, rest_http,
+};
 
 #[derive(Serialize)]
 pub struct AddUserRequest {
@@ -24,19 +28,21 @@ impl AddUserRequest {
         self.comment = Some(comment.to_string());
         self
     }
-}
 
-impl RestHttp for AddUserRequest {
-    type RequestData = AddUserRequest;
-    type ResponseData = AddUserResponse;
-    const METHOD: Method = Method::POST;
+    pub fn into_request(self) -> Request {
+        let body = serde_json::to_string_pretty(&json!({"data": self})).unwrap();
+        Request::json(Method::POST, "config/rest/ssh/v1/users".to_string()).body(body)
+    }
 
-    fn to_path_and_data(self) -> anyhow::Result<(String, Self::RequestData)> {
-        Ok(("config/rest/ssh/v1/users".to_string(), self))
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<AddUserResponse, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum AddUserResponse {
     /// Returned on AXIS OS 11.11.73.
@@ -77,23 +83,26 @@ impl SetUserRequest {
         self.properties.comment = Some(comment.to_string());
         self
     }
-}
 
-impl RestHttp for SetUserRequest {
-    type RequestData = SetUserProperties;
-    type ResponseData = SetUserResponse;
-    const METHOD: Method = Method::PATCH;
-
-    fn to_path_and_data(self) -> anyhow::Result<(String, Self::RequestData)> {
+    pub fn into_request(self) -> Request {
         let Self {
+            properties,
             username,
-            properties: data,
         } = self;
-        Ok((format!("config/rest/ssh/v1/users/{username}"), data))
+        let path = format!("config/rest/ssh/v1/users/{username}");
+        let body = serde_json::to_string_pretty(&json!({"data": properties})).unwrap();
+        Request::json(Method::PATCH, path).body(body)
+    }
+
+    pub async fn send(
+        self,
+        client: &(impl HttpClient + Sync),
+    ) -> Result<SetUserResponse, Error<rest::Error>> {
+        rest_http::send_request(client, self.into_request()).await
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct SetUserResponse(());
 
 // TODO: Consider creating new types for comment, username, and password.

crates/vapix/src/lib.rs
@@ -8,7 +8,6 @@ pub mod json_rpc;
 pub(crate) mod json_rpc_http;
 pub mod rest;
 pub mod rest_http;
-pub mod rest_http2;
 mod services;
 pub mod soap;
 pub mod soap_http;

crates/vapix/src/rest_http.rs
@@ -1,6 +1,6 @@
 //! Utilities for working with REST-style configuration APIs over HTTP.
 
-use std::{future::Future, marker::PhantomData};
+use std::marker::PhantomData;
 
 use anyhow::Context;
 use log::trace;
@@ -9,32 +9,15 @@ use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
 use crate::{
-    http::Error,
+    http::{Error, HttpClient, Request},
     rest,
-    rest::{parse_data, parse_data_lossless},
-    Client,
+    rest::parse_data_lossless,
 };
 
 // The device configuration API reports status codes both in the HTTP header and in the body.
 // TODO: Consider if there is any value in this.
 
 pub(crate) fn from_response<T>(
-    http_status: StatusCode,
-    text: reqwest::Result<String>,
-) -> Result<T, Error<rest::Error>>
-where
-    T: for<'a> Deserialize<'a>,
-{
-    let text = text
-        .with_context(|| format!("Could not fetch text, status was {http_status}"))
-        .map_err(Error::Transport)?;
-    if cfg!(debug_assertions) {
-        trace!("Received {http_status}: {text}");
-    }
-    Error::flat_result(parse_data(&text))
-}
-
-pub(crate) fn from_response_lossless<T>(
     http_status: StatusCode,
     text: reqwest::Result<String>,
 ) -> Result<T, Error<rest::Error>>
@@ -48,6 +31,17 @@ where
         trace!("Received {http_status}: {text}");
     }
     Error::flat_result(parse_data_lossless(&text))
+}
+
+pub(crate) async fn send_request<T>(
+    client: &(impl HttpClient + Sync),
+    request: Request,
+) -> Result<T, Error<rest::Error>>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    let response = client.execute(request).await.map_err(Error::Transport)?;
+    from_response(response.status, response.body)
 }
 
 pub struct RequestBuilder<T> {
@@ -71,64 +65,14 @@ impl<T> RequestBuilder<T> {
     }
 }
 
-impl<T> RestHttp for RequestBuilder<T>
+impl<T> RequestBuilder<T>
 where
-    T: for<'a> Deserialize<'a> + Send,
+    T: for<'a> Deserialize<'a> + Serialize + Send,
 {
-    type RequestData = Value;
-    type ResponseData = T;
-    const METHOD: Method = Method::POST;
-
-    fn to_path_and_data(self) -> anyhow::Result<(String, Self::RequestData)> {
-        let Self {
-            path,
-            data,
-            _phantom,
-        } = self;
-        Ok((path.to_string(), data))
-    }
-}
-
-pub trait RestHttp: Send + Sized {
-    type RequestData: Send + Serialize;
-    type ResponseData: for<'a> Deserialize<'a>;
-
-    const METHOD: Method;
-
-    fn to_path_and_data(self) -> anyhow::Result<(String, Self::RequestData)>;
-
-    fn send(
-        self,
-        client: &Client,
-    ) -> impl Future<Output = Result<Self::ResponseData, Error<rest::Error>>> + Send {
-        async move {
-            let (path, data) = self.to_path_and_data().map_err(Error::Request)?;
-            let json = json!({"data":data});
-            if cfg!(debug_assertions) {
-                trace!(
-                    "Sending {} to {path}: {}",
-                    Self::METHOD,
-                    serde_json::to_string(&json).unwrap()
-                );
-            }
-            let response = client
-                .request(Self::METHOD, &path)
-                .map_err(Error::Request)?
-                .json(&json)
-                .send()
-                .await
-                .context("Failed to send request")
-                .map_err(Error::Transport)?;
-            let status = response.status();
-            let text = response.text().await;
-
-            if cfg!(debug_assertions) {
-                if let Ok(text) = text.as_deref() {
-                    trace!("Received {status}: {text}");
-                }
-            }
-
-            from_response(status, text)
-        }
+    pub async fn send(self, client: &(impl HttpClient + Sync)) -> Result<T, Error<rest::Error>> {
+        let body = serde_json::to_string_pretty(&json!({"data": self.data}))
+            .map_err(|e| Error::Request(e.into()))?;
+        let request = Request::json(Method::POST, self.path.to_string()).body(body);
+        send_request(client, request).await
     }
 }

crates/vapix/src/rest_http2.rs
@@ -1,33 +1,0 @@
-//! Utilities for working with REST-style configuration APIs over HTTP.
-
-use std::future::Future;
-
-use serde::{Deserialize, Serialize};
-
-use crate::{
-    http::{Error, HttpClient, Request, Response},
-    rest,
-    rest_http::from_response_lossless,
-};
-
-// As a user of the request builders, I find having to import the correct trait annoying.
-// TODO: Consider consolidating traits or implementing send directly on the request types.
-
-pub trait RestHttp2: Send + Sized {
-    type ResponseData: for<'a> Deserialize<'a> + Serialize;
-
-    fn to_request(self) -> Request;
-
-    fn send(
-        self,
-        client: &(impl HttpClient + Sync),
-    ) -> impl Future<Output = Result<Self::ResponseData, Error<rest::Error>>> + Send {
-        async move {
-            let Response { status, body } = client
-                .execute(self.to_request())
-                .await
-                .map_err(Error::Transport)?;
-            from_response_lossless(status, body)
-        }
-    }
-}

commit 6f27514ac78b721727a706566a949f279f0e7ec2

feat(vapix)!: Add missing fields in add recording group response
The smoke tests have started failing because the response deserialization is not lossless. This is probably a regression in 622fa9dbd70547b54b4befc348af71eab0e72a58 which caused all config requests to take the lossless checking path.

crates/vapix/src/config/recording_group_1.rs
@@ -11,8 +11,48 @@ use crate::{
 };
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct CreateRecordingGroupResponse {
     pub id: String,
+    pub container_format: String,
+    pub description: String,
+    pub destinations: Vec<Destination>,
+    pub max_retention_time: u64,
+    pub nice_name: String,
+    pub post_duration: u64,
+    pub pre_duration: u64,
+    pub segment_duration: SegmentDuration,
+    pub segment_size: SegmentSize,
+    pub span_duration: u64,
+    pub stream_options: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Destination {
+    pub remote_object_storage: RemoteObjectStorage,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RemoteObjectStorage {
+    pub id: String,
+    #[serde(default)]
+    pub prefix: String,
+    #[serde(default)]
+    pub postfix: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct SegmentDuration {
+    pub max: u64,
+    pub target: u64,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct SegmentSize {
+    pub max: u64,
+    pub target: u64,
 }
 
 #[derive(Debug)]